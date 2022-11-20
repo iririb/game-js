ca2004ca687f8d86b294e372b32c6877e1c868c5
iririb <eshamibrahim94@gmail.com>
2022-11-20 20:49:04 +0800

    Basic game screen

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
+"use strict"
+
+const WIDTH = 720;
+const HEIGHT = 540;
+
+const MESH = 24;
+
+var gStyle = "#00ffff";
+var gX = WIDTH / 2;
+var gY = HEIGHT - MESH * 3;
+var gScore = 0;
+var gLife = 3;
+
 // called once browser run
 window.onload = function () {
 
     let g = document.getElementById("main").getContext("2d");
 
-    g.fillStyle = "#0000ff";
-    g.fillRect(20, 40, 60, 80);
+    // game background
+    g.fillStyle = "#ffffff";
+    g.fillRect(0, 0, WIDTH, HEIGHT);
+
+    // game board
+    g.fillStyle = "#000000";
+    g.fillRect(MESH, MESH, WIDTH - MESH * 2, HEIGHT - MESH * 2);
+
+    // player
+    g.fillStyle = gStyle;
+    g.fillRect(gX, gY, MESH, MESH);
 
     g.font = "36px monospace";
     g.fillStyle = "#ffffff";
-    g.fillText("hello world", 100, 100);
+    g.fillText("Score:" + gScore, MESH * 2, MESH * 2.5);
+    g.fillText("Life:" + gLife, MESH * 23, MESH * 2.5);
 
 
 }
