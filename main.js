9a7ccfa280754fd16a9c13f52be4d833fb6cc552
iririb <eshamibrahim94@gmail.com>
2022-11-21 02:12:58 +0800

    Game logic and object

--- main.js
+++ main.js
@@ -13,7 +13,48 @@ var gScore = 0;
 var gLife = 3;
 var gKey = new Uint8Array(0x100);
 var gTimer;
+var gBall = [];
+var gColor = ["#ff0000", "#ff00ff", "#00ff00", "#ffff00"];
 
+class Ball {
+    constructor(c) {
+        this.mX = WIDTH / 2;
+        this.mY = MESH;
+        let a = Math.random() * 2.5 + (Math.PI - 2.5) / 2;
+        this.mDX = Math.cos(a);
+        this.mDY = Math.sin(a);
+        this.mStyle = gColor[c];
+    }
+    draw(g) {
+        g.fillStyle = this.mStyle;
+        g.fillRect(this.mX - MAG, this.mY - MAG, MAG * 2, MAG * 2);
+    }
+    tick() {
+        if (IsInRect(this.mX, this.mY, gX, gY, MESH, MESH)) {
+            return (true);
+        }
+
+        this.mX += this.mDX;
+        this.mY += this.mDY;
+        if (this.mX < MESH || this.mX > WIDTH - MESH) {
+            this.mDX = -this.mDX;
+            this.mX += this.mDX;
+            gScore++;
+        }
+        if (this.mY < MESH || this.mY > HEIGHT - MESH) {
+            this.mDY = -this.mDY;
+            this.mY += this.mDY;
+            gScore++;
+        }
+        return (false);
+
+    }
+}
+
+function IsInRect(x, y, rx, ry, rw, rh) {
+    return (rx < x && x < rx + rw &&
+        ry < y && y < ry + rh)
+}
 
 function draw() {
 
@@ -31,14 +72,33 @@ function draw() {
     g.fillStyle = gStyle;
     g.fillRect(gX, gY, MESH, MESH);
 
+
+    // enemy
+    for (let b of gBall) {
+        b.draw(g)
+    }
+
     g.font = "36px monospace";
     g.fillStyle = "#ffffff";
     g.fillText("Score:" + gScore, MESH * 2, MESH * 2.5);
     g.fillText("Life:" + gLife, MESH * 23, MESH * 2.5);
 
+    if (gLife <= 0) {
+        g.fillText("GAME OVER", WIDTH / 2 - MESH * 3, HEIGHT / 2)
+    }
+
+}
+
+function start() {
+    for (let i = 0; i < 8; i++) {
+        gBall.push(new Ball(i % 3));
+    }
 }
 
 function tick() {
+    if (gLife <= 0) {
+        return;
+    }
     // if (gKey[37]) {
     //     gX -= MAG;
     // } 
@@ -46,10 +106,21 @@ function tick() {
     //     gX += MAG;
     // }
     // above can be written as below
-    gX -= gKey[37] * MAG;
-    gX += gKey[39] * MAG;
-    gY -= gKey[38] * MAG;
-    gY += gKey[40] * MAG;
+    gX = Math.max(MESH, gX - gKey[37] * MAG);
+    gX = Math.min(WIDTH - MESH * 2, gX + gKey[39] * MAG);
+    gY = Math.max(MESH, gY - gKey[38] * MAG);
+    gY = Math.min(HEIGHT - MESH * 2, gY + gKey[40] * MAG);
+
+    for (let i = 0; i < 4 + gScore / 20; i++) { // call 4 time to increase speed of ball
+        // for (let b of gBall) {
+        for (let i = gBall.length - 1; i >= 0; i--) {
+            if (gBall[i].tick()) {
+                gLife--;
+                gStyle = gBall[i].mStyle;
+                gBall.splice(i, 1);
+            }
+        }
+    }
 
 }
 function onPaint() {
@@ -81,6 +152,8 @@ window.onkeyup = function (ev) {
 }
 // called once browser run
 window.onload = function () {
+
+    start();
 
     // called function, depend on cpu perfomace the hertz is different
     requestAnimationFrame(onPaint);
